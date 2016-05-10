371e65192d874506331e16bfb533b7f6f4682fdc
Nikola Metulev <nikolame@microsoft.com>
2016-05-10 00:33:48 -0700

    fixed test

---
+++ src/MobileApps/MyDriving/MyDriving.UITests/Tests/SimpleTest.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.UITest;
+using Xamarin.UITest.Android;
+
+namespace MyDriving.UITests.Tests
+{
+    [TestFixture]
+    public class SimpleTest
+    {
+        AndroidApp app;
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = ConfigureApp.Android.ApkFile("../../MyDriving.Android/bin/Release/com.microsoft.mydriving-Signed").StartApp();
+        }
+        
+    }
+}
